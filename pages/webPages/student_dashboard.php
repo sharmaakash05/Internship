351b3709f36b7a204436992a3bfd4957d88126ed
sharmaakash05 <64541735+sharmaakash05@users.noreply.github.com>
2021-05-28 13:59:31 +0530

    Add files via upload

---
+++ pages/webPages/student_dashboard.php
@@ -0,0 +1,13 @@
+<div class="navbar fixed-top bg-white p box-shadow">
+    <div class="row nav-scroller">
+       <div class="col-sm-3 text-left nav-li" onclick="loadDoc('content','pages/webPages/student_dashboard_home.php')">
+        <strong> Dashboard </strong>
+       </div>
+       <div class="col-sm-3 text-left nav-li" onclick="loadDoc('content','pages/webPages/student_dashboard_internships.php')">
+        View applied Internships
+       </div>
+       <div class="col-sm-3 text-left nav-li" onclick="loadDoc('content','pages/webPages/internlist.php')">
+        Apply for new Internship
+       </div>
+    </div>
+</div>
